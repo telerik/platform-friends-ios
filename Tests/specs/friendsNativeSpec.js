0fce2bdc4f3ad9826e4814e1331c5cd0831c883b
shoerob <shoerob@gmail.com>
2014-01-16 12:47:38 -0600

    removed android tests

--- Tests/specs/friendsNativeSpec.js
+++ Tests/specs/friendsNativeSpec.js
@@ -18,20 +18,6 @@ spec(function() {
 				textView: { class: 'UITextView' },
 				postButton: { class: 'UINavigationButton', properties: { 'currentTitle': 'Post' } }
 			}
-		},
-		android: {
-			login: {
-				usernameField: { class: 'android.widget.EditText', properties: { 'hint': 'Username' } },
-				passwordField: { class: 'android.widget.EditText', properties: { 'hint': 'Password' } },
-				loginButton: { class: 'android.widget.Button', properties: { 'text': 'Login' } }
-			},
-			activities: {
-				addButton: { class: 'com.android.internal.view.menu.ActionMenuItemView' },
-			},
-			activity: {
-				textView: { class: 'android.widget.EditText' },
-				postButton: { class: 'com.android.internal.view.menu.ActionMenuItemView' }
-			}
 		}
 	};
 
@@ -39,10 +25,6 @@ spec(function() {
 		'Given Friends is running': {
 			'ios': [
 				ios.launch('tsfriends://')
-			],
-			'android': [
-				android.launch('com.telerik.app'),
-				android.wait(1000)
 			]
 		},
 		'And is logged in': {
@@ -52,33 +34,18 @@ spec(function() {
 				ios.setText(queries.ios.login.passwordField, "telerik", false),
 				ios.tap(queries.ios.login.loginButton),
 				ios.wait(3000)
-			],
-			'android': [
-				android.setText(queries.android.login.usernameField, "seth"),
-				android.setText(queries.android.login.passwordField, "333333"),
-				android.tap(queries.android.login.loginButton),
-				android.wait(3000)
 			]
 		},
 		'When add is tapped': {
 			'ios': [
 				ios.wait(2000),
 				ios.tap(queries.ios.activities.addButton)
-			],
-			'android': [
-				android.wait(2000),
-				android.tap(queries.android.activities.addButton),
-				android.wait(1000)
 			]
 		},
 
 		'And something on my mind is input': {
 			'ios': [
 				ios.setText(queries.ios.activity.textView, 'Hello World', true)
-			],
-			'android': [
-				android.setText(queries.android.activity.textView, 'Hello World'),
-				android.wait(1000)
 			]
 		},
 
@@ -86,30 +53,17 @@ spec(function() {
 			'ios': [
 				ios.tap(queries.ios.activity.postButton),
 				ios.wait(3000)
-			],
-			'android': [
-				android.tap(queries.android.activity.postButton),
-				android.wait(3000)
 			]
 		},
 
 		'And logout is tapped': {
 			'ios': [
 				ios.tap(queries.ios.activities.logoutButton)
-			],
-			'android': [
-				android.pressBackButton(),
-				android.wait(1000)
 			]
 		},
 		'Then the Activities screen should be displayed' : {
 			'ios': [
 				ios.getPropertyValue(queries.ios.globals.navigationBar, 'topItem.title', function(result) {
-					assert(result).equals('Activities');
-				})
-			],
-			'android': [
-				android.getPropertyValue({ class: 'android.widget.TextView', index: 0 }, 'text', function (result) {
 					assert(result).equals('Activities');
 				})
 			]
@@ -121,21 +75,11 @@ spec(function() {
 				ios.getPropertyValue([{ class: 'UITableViewCell', index: 3 }, { class: 'UILabel', index: 2 }], 'text', function(value) {
 					assert(value).equals('Hello World');
 				})
-			],
-			'android': [
-				android.getPropertyValue({ class: 'android.widget.TextView', properties: { 'text': 'Hello World' } }, 'text', function (value) {
-					assert(value).equals('Hello World');
-				})
 			]
 		},
 		'Then the login screen should be displayed': {
 			'ios': [
 				ios.getPropertyValue(queries.ios.login.loginButton, 'text', function(value) {
-					assert(value).equals('Login');
-				})
-			],
-			'android': [
-				android.getPropertyValue(queries.android.login.loginButton, 'text', function (value) {
 					assert(value).equals('Login');
 				})
 			]
