97f33113ab7f5ff144a44b4f5cd924fb6e28e13b
shoerob <shoerob@gmail.com>
2014-01-22 17:54:44 -0600

    updated tests for native friends app

--- Tests/specs/friendsNativeSpec.js
+++ Tests/specs/friendsNativeSpec.js
@@ -18,6 +18,20 @@ spec(function() {
 				textView: { class: 'UITextView' },
 				postButton: { class: 'UINavigationButton', properties: { 'currentTitle': 'Post' } }
 			}
+		},
+		android: {
+			login: {
+				usernameField: { class: 'android.widget.EditText', properties: { 'hint': 'Username' } },
+				passwordField: { class: 'android.widget.EditText', properties: { 'hint': 'Password' } },
+				loginButton: { class: 'android.widget.Button', properties: { 'text': 'Login' } }
+			},
+			activities: {
+				addButton: { class: 'com.android.internal.view.menu.ActionMenuItemView' },
+			},
+			activity: {
+				textView: { class: 'android.widget.EditText' },
+				postButton: { class: 'com.android.internal.view.menu.ActionMenuItemView' }
+			}
 		}
 	};
 
@@ -25,27 +39,47 @@ spec(function() {
 		'Given Friends is running': {
 			'ios': [
 				ios.launch('tsfriends://')
+			],
+			'android': [
+				android.launch('com.telerik.app'),
+				android.wait(1000)
 			]
 		},
 		'And is logged in': {
 			'ios': [
-				// NOTE: Update the following two lines to use an existing Telerik Backend Services account and password
-				ios.setText(queries.ios.login.usernameField, "USERNAME", false),
-				ios.setText(queries.ios.login.passwordField, "PASSWORD", false),
+				// NOTE: Update the following two lines to use an existing everlive account and password
+				ios.setText(queries.ios.login.usernameField, "USERNAME"),
+				ios.setText(queries.ios.login.passwordField, "PASSWORD"),
 				ios.tap(queries.ios.login.loginButton),
 				ios.wait(3000)
+			],
+			'android': [
+				// NOTE: Update the following two lines to use an existing everlive account and password
+				android.setText(queries.android.login.usernameField, "USERNAME"),
+				android.setText(queries.android.login.passwordField, "PASSWORD"),
+				android.tap(queries.android.login.loginButton),
+				android.wait(3000)
 			]
 		},
 		'When add is tapped': {
 			'ios': [
 				ios.wait(2000),
 				ios.tap(queries.ios.activities.addButton)
+			],
+			'android': [
+				android.wait(2000),
+				android.tap(queries.android.activities.addButton),
+				android.wait(1000)
 			]
 		},
 
 		'And something on my mind is input': {
 			'ios': [
-				ios.setText(queries.ios.activity.textView, 'Hello World', true)
+				ios.typeText(queries.ios.activity.textView, 'Hello World')
+			],
+			'android': [
+				android.setText(queries.android.activity.textView, 'Hello World'),
+				android.wait(1000)
 			]
 		},
 
@@ -53,17 +87,30 @@ spec(function() {
 			'ios': [
 				ios.tap(queries.ios.activity.postButton),
 				ios.wait(3000)
+			],
+			'android': [
+				android.tap(queries.android.activity.postButton),
+				android.wait(3000)
 			]
 		},
 
 		'And logout is tapped': {
 			'ios': [
 				ios.tap(queries.ios.activities.logoutButton)
+			],
+			'android': [
+				android.pressBackButton(),
+				android.wait(1000)
 			]
 		},
 		'Then the Activities screen should be displayed' : {
 			'ios': [
 				ios.getPropertyValue(queries.ios.globals.navigationBar, 'topItem.title', function(result) {
+					assert(result).equals('Activities');
+				})
+			],
+			'android': [
+				android.getPropertyValue({ class: 'android.widget.TextView', index: 0 }, 'text', function (result) {
 					assert(result).equals('Activities');
 				})
 			]
@@ -75,11 +122,22 @@ spec(function() {
 				ios.getPropertyValue([{ class: 'UITableViewCell', index: 3 }, { class: 'UILabel', index: 2 }], 'text', function(value) {
 					assert(value).equals('Hello World');
 				})
+			],
+			'android': [
+				android.wait(2000),
+				android.getPropertyValue({ class: 'android.widget.TextView', properties: { 'text': 'Hello World' } }, 'text', function (value) {
+					assert(value).equals('Hello World');
+				})
 			]
 		},
 		'Then the login screen should be displayed': {
 			'ios': [
 				ios.getPropertyValue(queries.ios.login.loginButton, 'text', function(value) {
+					assert(value).equals('Login');
+				})
+			],
+			'android': [
+				android.getPropertyValue(queries.android.login.loginButton, 'text', function (value) {
 					assert(value).equals('Login');
 				})
 			]
@@ -99,7 +157,7 @@ spec(function() {
 			step('When add is tapped');
 			step('And something on my mind is input');
 			step('And post is tapped');
-			// step('Then the activity should be posted');
+			step('Then the activity should be posted');
 		});
 
 		test("Logout returns to login screen", function() {
@@ -110,3 +168,4 @@ spec(function() {
 		});
 	}, stepRepository);
 });
+
